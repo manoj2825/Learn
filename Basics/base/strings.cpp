b8776b4a8f3bd27c84b4b5e16c0b2bd369eeeacb
Manoj S <smanoj@ssdiblr.sharp.co.in>
2024-08-13 21:42:42 +0530

    asdf

--- Basics/base/strings.cpp
+++ Basics/base/strings.cpp
@@ -4,7 +4,12 @@
 
 using namespace std;
 
-
+void fun()
+{
+    static int x=10;
+    x++;
+    cout<<x<<endl;
+}
 
 int main()
 {
@@ -15,4 +20,13 @@ int main()
 
     int arr[]={1,2,4,4,5,5};
     cout<<&arr[3]<<endl;
+
+    int i =5;
+    int j =++i;
+    
+    cout<<"i : "<<i<<endl;
+    cout<<"j : "<<j<<endl;
+
+    fun();//11
+    fun();//12
 }
